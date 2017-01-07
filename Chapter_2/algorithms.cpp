2ea01590fa19a2b5af01ffe00763cbb45d534fad
Michael <anthony_michael_wilson@yahoo.com>
2017-01-07 12:30:39 -0800

    made a quick update to the printVector function

--- Chapter_2/algorithms.cpp
+++ Chapter_2/algorithms.cpp
@@ -67,7 +67,7 @@ void printArray(int array[], int size) {
  * @param vect The vector to print */
 void printVector(vector<int> vect) {
  	for(int i = 0; i < vect.size(); i++)
- 		cout << vect[i] << " ";
+ 		cout << vect[i] << "-> ";
  	cout << endl;
 }
 
